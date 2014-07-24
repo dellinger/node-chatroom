6a175100c744cc5426303b0c554fa8720b26b110
David Ellinger <david.ellinger@me.com>
2014-07-23 21:09:26 -0700

    Added static files and created chat_server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var http = require('http');
 var fs = require('fs');
 var path = require('path');
 var mime = require('mime');
+var chatServer = require('./lib/chat_server');
 
 var cache = {};
 
@@ -15,14 +16,14 @@ function send404(response) {
 function sendFile(response, filePath, fileContents) {
 	response.writeHead(
 		200, 
-		{"content-type" : mime.lookup(path.basename(filepath))}
+		{"content-type" : mime.lookup(path.basename(filePath))}
 	);
 	response.end(fileContents);
 }
 
-function serveStatic(response, cache, abspath) {
-	if (cache[abspath]) {
-		sendFile(response, abspath, cache[abspath]);
+function serveStatic(response, cache, absPath) {
+	if (cache[absPath]) {
+		sendFile(response, absPath, cache[absPath]);
 	} else {
 		fs.exists(absPath, function(exists) {
 			if(exists) {
@@ -52,3 +53,9 @@ var server = http.createServer(function(request, response) {
 	var absPath = './' + filePath;
 	serveStatic(response, cache, absPath);
 });
+
+server.listen(3000, function(){
+	console.log("Server listening on port 3000")
+})
+
+chatServer.listen(server);
